68cd64338d1955725ec1f9b66a23b1441fb89a7c
Yushchenko <MYushchenko@kiev.luxoft.com>
2016-10-11 18:49:42 +0300

    Add material

--- src/vendor.ts
+++ src/vendor.ts
@@ -9,3 +9,6 @@ import '@angular/router';
 import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
+
+//Material theme
+import '@angular/material/core/theming/prebuilt/purple-green.css';
